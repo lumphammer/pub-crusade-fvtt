707d110743da0d57acb56045730890e744d8574e
Neil de Carteret <git@lumphammer.net>
2025-05-13 16:50:20 +0100

    delete test function

--- src/PubCrusadeActor.ts
+++ src/PubCrusadeActor.ts
@@ -2,18 +2,6 @@
 // that seem to come out here
 /* eslint "@typescript-eslint/explicit-function-return-type": "error" */
 
-import { assertCharacterActor } from "./character";
-
-// just a type test
-async function _setOrder(
-  actor: Actor.Implementation,
-  order: string,
-): Promise<void> {
-  assertCharacterActor(actor);
-  // @ts-expect-error this should error on foo
-  await actor.update({ system: { order, foo: 5 } });
-}
-
 export class PubCrusadeActor<
   SubType extends Actor.SubType,
 > extends Actor<SubType> {
